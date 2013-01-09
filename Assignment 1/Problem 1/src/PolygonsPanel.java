96c8927c106db6fd676b3796d9831b8b6e62206c
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-01-08 22:27:23 -0400

    Assignment 1 Problem 1 Added check of existing file, error handling when opening to write file, save polygon to file at the time of completing it; keyPressed and mouseClick events handling taken out of listener class

--- Assignment 1/Problem 1/src/PolygonsPanel.java
+++ Assignment 1/Problem 1/src/PolygonsPanel.java
@@ -2,6 +2,8 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.swing.JPanel;
@@ -28,8 +30,13 @@ public class PolygonsPanel extends JPanel {
         this.addMouseListener(listener);
 
         file = new File("polygons.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
 
-        completeness = false;
+        completeness = true;
         polygons = new ArrayList<>();
 
         nPoints = 0;
@@ -52,17 +59,31 @@ public class PolygonsPanel extends JPanel {
 
     }
 
-    private void saveToFile(Polygon polygon) throws Exception {
-        PrintWriter output = new PrintWriter(file);
-        for (int i = 0; i < polygon.npoints; i++) {
-            output.println(polygon.xpoints[i] + "\t" + polygon.ypoints[i]);
+    private void saveToFile(Polygon polygon) {
+        PrintWriter output = null;
+
+        try {
+            //try open file
+            output = new PrintWriter(file);
+
+            //write all points of polygon into file and add "-1 -1" after polygon
+            for (int i = 0; i < polygon.npoints; i++) {
+                output.println(polygon.xpoints[i] + "\t" + polygon.ypoints[i]);
+            }
+            output.println("-1\t-1");
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex);
         }
-        output.println("-1\t-1");
+
+        output.close();
     }
 
     private void completePolygon() {
-        polygons.add(new Polygon(xPoints, yPoints, nPoints));
         completeness = true;
+
+        Polygon tmpPolygon = new Polygon(xPoints, yPoints, nPoints);
+        polygons.add(tmpPolygon);
+        saveToFile(tmpPolygon);
 
         nPoints = 0;
         xPoints = new int[100];
@@ -73,53 +94,58 @@ public class PolygonsPanel extends JPanel {
 
     private void quit(int code) {
         if (!completeness) {
-            polygons.add(new Polygon(xPoints, yPoints, nPoints));
-        }
-        try {
-            saveToFile(polygons.get(polygons.size()));
-        } catch (Exception ex) {
-            System.out.println(ex);
+            Polygon tmpPolygon = new Polygon(xPoints, yPoints, nPoints);
+            saveToFile(tmpPolygon);
         }
 
         System.exit(code);
+    }
+
+    private void keyPress(KeyEvent evt) {
+        if (evt.getKeyCode() == KeyEvent.VK_C) {
+            completePolygon();
+        }
+//        if (e.getKeyCode() == KeyEvent.VK_Q) {
+        if (evt.getKeyChar() == 'q' || evt.getKeyChar() == 'Q') {
+            quit(0);
+        }
+    }
+
+    private void mouseClick(MouseEvent evt) {
+        if (completeness) {
+            completeness = false;
+        }
+
+        xPoints[nPoints] = evt.getPoint().x;
+        yPoints[nPoints] = evt.getPoint().y;
+        nPoints++;
+
+        repaint();
     }
 
     private class InputListener implements KeyListener, MouseListener {
 
         @Override
         public void keyPressed(KeyEvent e) {
-            System.out.println(e.getKeyChar() + "\t" + e.getKeyCode());
-            if (e.getKeyCode() == KeyEvent.VK_C) {
-                completePolygon();
-            }
-//            if (e.getKeyCode() == KeyEvent.VK_Q) {
-            if (e.getKeyChar() == 'q' || e.getKeyChar() == 'Q') {
-                quit(0);
-            }
+            keyPress(e);
         }
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            completeness = false;
-
-            xPoints[nPoints] = e.getPoint().x;
-            yPoints[nPoints] = e.getPoint().y;
-            nPoints++;
-
-            repaint();
+            mouseClick(e);
         }
 
         //not implemented
-        @Override
-        public void mousePressed(MouseEvent e) {
-        }
-
         @Override
         public void keyTyped(KeyEvent e) {
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
         }
 
         @Override
