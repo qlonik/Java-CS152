8631832550ea99ca82a35303ee9e6fad14cea5d2
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-03-15 20:28:39 -0300

    Assignment 7 - Server side. Rename of Storage.java to AccountStorage.java

---
+++ Assignment 7/server/src/AccountStorage.java
@@ -0,0 +1,17 @@
+
+import java.util.ArrayList;
+
+
+public class AccountStorage extends ArrayList<Account> {
+
+    @Override
+    public boolean add(Account e) {
+        return super.add(e);
+    }
+
+//    public boolean remove(int accountID) {
+//        
+//    }
+
+    
+}
