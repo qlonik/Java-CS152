4da636d48374df2c7bfc1dfaea849cc09abb8907
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-03-16 01:43:58 -0300

    Assignment 7 - Server side. This class has methods that do work with each particular Account class. It saves all deposit/withdraw operations to log file.

--- Assignment 7/server/src/Account.java
+++ Assignment 7/server/src/Account.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /*
  * Account.java     Nikita Volodin
@@ -13,6 +15,7 @@ import java.text.NumberFormat;
 public class Account {
 
     final private String PATH = "./users/";
+    File outFile = null;
     private FileWriter out = null;
     private int accountID = 0;
     private double money = 0;
@@ -32,22 +35,29 @@ public class Account {
     public Account(String account, String startingDeposit) {
         this(Integer.parseInt(account), Double.parseDouble(startingDeposit));
     }
-    
+
     /**
      * Returns ID of this account in int form
-     * 
+     *
      * @return account id
      */
     public int getAccountID() {
         return accountID;
     }
 
+    private String getTime() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy HH:mm:ss");
+        
+        return sdf.format(cal.getTime());
+    }
+
     /**
      * Returns amount of money on account
-     * 
+     *
      * @return current balance of account
      */
-    public double getBalance() {
+    public synchronized double getBalance() {
         return money;
     }
 
@@ -57,7 +67,7 @@ public class Account {
      * @param amount amount to add
      * @return new amount of money on account
      */
-    public double deposit(double amount) {
+    public synchronized double deposit(double amount) {
         try {
             saveToFile("DEPOSIT", amount);
             money += amount;
@@ -74,7 +84,7 @@ public class Account {
      * @param amount amount to withdraw
      * @return new amount of money on account
      */
-    public double withdraw(int amount) {
+    public synchronized double withdraw(double amount) {
         if (amount <= money) {
             try {
                 saveToFile("WITHDRAW", amount);
@@ -89,29 +99,21 @@ public class Account {
         }
     }
 
-    public boolean deleteAccount() {
-        File outFile = new File(PATH + accountID);
-        if (outFile.delete()) {
-            return true;
-        } else {
-            return false;
-        }
+    public void deleteAccount() {
+        outFile.delete();
     }
 
     private void createFile() throws IOException {
-        out = null;
-        File outFile = new File(PATH + accountID);
+        outFile = new File(PATH + accountID);
         outFile.delete();           //delete previous file and create new
         outFile.createNewFile();    //each time when we create new instance
-
         out = new FileWriter(outFile, true);
 
         NumberFormat fmt = NumberFormat.getCurrencyInstance();
 
         if (out != null) {
-//            out.write("Name: " + name + "\t\t\tAccount ID: " + accountID + "\n\n");
             out.write("Account ID: " + accountID + "\n\n");
-            out.write("Starting deposit: " + fmt.format(money) + "\n");
+            out.write(getTime() + "\tStarting balance: " + fmt.format(money) + "\n");
         }
 
         out.close();
@@ -120,13 +122,16 @@ public class Account {
     private void saveToFile(String type, double amount) throws IOException {
         NumberFormat fmt = NumberFormat.getCurrencyInstance();
 
+        out = new FileWriter(outFile, true);
+
         if (out != null) {
             switch (type) {
                 case "DEPOSIT":
-                    out.write("DEPOSIT:\t\t " + fmt.format(amount));
+                    out.write(getTime() + "\tDEPOSIT:\t" + fmt.format(amount) + "\n");
                     break;
+
                 case "WITHDRAW":
-                    out.write("WITHDRAW\t\t:" + fmt.format(amount));
+                    out.write(getTime() + "\tWITHDRAW:\t" + fmt.format(amount) + "\n");
                     break;
             }
         }
