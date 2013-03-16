b895f52111da2942ee3d24a4080ad4287136412f
Никита Володин [Nikita Volodin] <volodin.n@gmail.com>
2013-03-16 01:26:42 -0300

    Assignment 7 - Server side. Added cases for operational methods in run method. Added operational methods that do stuff.

--- Assignment 7/server/src/Connection.java
+++ Assignment 7/server/src/Connection.java
@@ -56,90 +56,216 @@ public class Connection extends Thread {
     private void send(ArrayList<String> data) {
         String msg = "";
 
-        for (int i = 0; i < data.size(); i++) {
-            msg += data.get(i) + ":";
-        }
+        if (data != null) {
+            for (int i = 0; i < data.size(); i++) {
+                msg += data.get(i) + ":";
+            }
 
-        msg = msg.substring(0, msg.length() - 1) + ";";
+            msg = msg.substring(0, msg.length() - 1) + ";";
+        }
 
         output.print(msg);
     }
 
-    private void createAccount(ArrayList<String> data) {
+    /*
+     * All operational methods (create, delete, deposit, withdraw, inquire) say to parent (Server) to do stuff
+     * Server class asks storage class to create or generate error
+     * And later all operational methods parse result from storage class (passed through Server class) and 
+     * generate answer to client
+     */
+    
+    private boolean createAccount(ArrayList<String> data) {
+        boolean result;
         ArrayList<String> toSend = new ArrayList<>();
 
-        if (parent.createAccount(data)) {
+        Account created = parent.createAccount(data);
+        if (created != null) {
+            result = true;
+
             toSend.add("SUCCESS");
-            toSend.add(data.get(2));
+            toSend.add("" + created.getBalance());
         } else {
+            result = false;
+
             toSend.add("FAIL");
             toSend.add("2");
         }
 
         send(toSend);
+        return result;
     }
 
-    private void deleteAccount(ArrayList<String> data) {
-//        throw new UnsupportedOperationException("Not yet implemented");
+    private boolean deleteAccount(ArrayList<String> data) {
+        boolean result;
+        ArrayList<String> toSend = new ArrayList<>();
+
+        Account removed = parent.deleteAccount(data);
+        if (removed != null) {
+            result = true;
+
+            toSend.add("SUCCESS");
+            toSend.add("" + removed.getBalance());
+        } else {
+            result = false;
+
+            toSend.add("FAIL");
+            toSend.add("1");
+        }
+
+        send(toSend);
+        return result;
     }
 
-    private void deposit(ArrayList<String> data) {
-//        throw new UnsupportedOperationException("Not yet implemented");
+    private boolean deposit(ArrayList<String> data) {
+        boolean result;
+        ArrayList<String> toSend = new ArrayList<>();
+
+        Account deposited = parent.deposit(data);
+        if (deposited != null) {
+            result = true;
+
+            toSend.add("SUCCESS");
+            toSend.add("" + deposited.getBalance());
+        } else {
+            result = false;
+
+            toSend.add("FAIL");
+            toSend.add("1");
+        }
+
+        send(toSend);
+        return result;
     }
 
-    private void withdraw(ArrayList<String> data) {
-//        throw new UnsupportedOperationException("Not yet implemented");
+    private boolean withdraw(ArrayList<String> data) {
+        boolean result;
+        ArrayList<String> toSend = new ArrayList<>();
+
+        Object[] withdrawed = parent.withdraw(data);
+        Account acc = (Account) withdrawed[1];
+        boolean moneyLack = (boolean) withdrawed[0];
+
+        if (acc != null && !moneyLack) {
+            result = true;
+
+            toSend.add("SUCCESS");
+            toSend.add("" + acc.getBalance());
+        } else if (acc == null) {
+            result = false;
+
+            toSend.add("FAIL");
+            toSend.add("1");
+        } else {
+            result = false;
+
+            toSend.add("FAIL");
+            toSend.add("3");
+        }
+
+        send(toSend);
+        return result;
     }
 
-    private void inquire(ArrayList<String> data) {
-//        throw new UnsupportedOperationException("Not yet implemented");
+    private boolean inquire(ArrayList<String> data) {
+        boolean result;
+        ArrayList<String> toSend = new ArrayList<>();
+
+        Account inquired = parent.inquire(data);
+        if (inquired != null) {
+            result = true;
+
+            toSend.add("SUCCESS");
+            toSend.add("" + inquired.getBalance());
+        } else {
+            result = false;
+
+            toSend.add("FAIL");
+            toSend.add("1");
+        }
+
+        send(toSend);
+        return result;
     }
 
     @Override
     public void run() {
         try {
             ArrayList<String> data = receive();
-
             String command = data.get(0);
-            ArrayList<String> toSend = null;
 
             switch (command) {
                 case "CREATE":
-                    System.out.println("CREATING ACC");
-                    createAccount(data);
                     //create new account
+                    System.out.print("CLIENT " + s.getRemoteSocketAddress()
+                            + "\t\tCREATING ACCOUNT ID#" + data.get(1));
+                    if (createAccount(data)) {
+                        System.out.println("\t\tSuccessful operation");
+                    } else {
+                        System.out.println("\t\tFailed to create. This account number "
+                                + "already exist");
+                    }
                     break;
+
                 case "DELETE":
-                    System.out.println("DELETING ACC");
-                    deleteAccount(data);
                     //delete account
+                    System.out.print("CLIENT " + s.getRemoteSocketAddress()
+                            + "\t\tDELETING ACCOUNT ID#" + data.get(1));
+                    if (deleteAccount(data)) {
+                        System.out.println("\t\tSuccessful operation");
+                    } else {
+                        System.out.println("\t\tFailed to delete. This account "
+                                + "does not exist");
+                    }
                     break;
+
                 case "DEPOSIT":
-                    System.out.println("DEPOSITING MONEY");
-                    deposit(data);
                     //deposit amount to account
+                    System.out.print("CLIENT " + s.getRemoteSocketAddress()
+                            + "\t\tDEPOSITING TO ACCOUNT ID#" + data.get(1));
+                    if (deposit(data)) {
+                        System.out.println("\t\tSuccessful operation");
+                    } else {
+                        System.out.println("\t\tFailed to deposit. This account "
+                                + "does not exist");
+                    }
                     break;
+
                 case "WITHDRAW":
-                    System.out.println("WITHDRAWING MONEY");
-                    withdraw(data);
                     //withdraw amount from account
+                    System.out.print("CLIENT " + s.getRemoteSocketAddress()
+                            + "\t\tWITHDRAWING FROM ACCOUNT ID#" + data.get(1));
+                    if (withdraw(data)) {
+                        System.out.println("\t\tSuccessful operation");
+                    } else {
+                        System.out.println("\t\tFailed to withdraw. ");
+                        //#TODO different messages dependant upon code of fail
+                    }
                     break;
+
                 case "INQUIRE":
-                    System.out.println("INQUIRING");
-                    inquire(data);
-                    //send amount on account
+                    //inquire amount on account
+                    System.out.print("CLIENT " + s.getRemoteSocketAddress()
+                            + "\t\tINQUIRING ACCOUNT ID#" + data.get(1));
+                    if (inquire(data)) {
+                        System.out.println("\t\tSuccessful operation");
+                    } else {
+                        System.out.println("\t\tFailed to inquire. "
+                                + "This account does not exist");
+                    }
                     break;
+
                 default:
-                    //command does not exist message
+                    //#TODO command does not exist message
                     break;
             }
 
             s.close();
             System.out.println("Client from " + s.getRemoteSocketAddress() + " disconnected");
         } catch (IOException ex) {
-            System.err.println(System.currentTimeMillis() + "\t" + ex);
+            System.err.println(parent.getTime() + "\t" + ex);
         } catch (Exception ex) {
-            System.err.println(System.currentTimeMillis() + "\t" + ex);
+            System.out.println("exception on line 191 of connection.java");
+            System.err.println(parent.getTime() + "\t" + ex);
         }
     }
 }
